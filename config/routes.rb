d69d2675c7bceee13dab95ea99ec529fb97dbadc
Jasmine Dang <ngandth.1992@gmail.com>
2020-05-17 15:20:58 +0800

    flat controller done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'flats/index'
+  get 'flats/show'
+  get 'flats/new'
+  get 'flats/create'
+  get 'flats/edit'
+  get 'flats/update'
+  get 'flats/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :flats
 end
